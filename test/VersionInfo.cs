39a520afd9ed4f8fb19f8a8a4136ba4759a87c18
jsakamoto <junichi.jp@gmail.com>
2025-03-19 08:47:42 +0900

    Update package references to 10.0.0-preview.2

--- test/VersionInfo.cs
+++ test/VersionInfo.cs
@@ -1,5 +1,5 @@
 namespace Toolbelt.AspNetCore.Blazor.Minimum.Templates.Test;
 internal static class VersionInfo
 {
-    internal const string VersionText = "10.0.100-preview.1";
+    internal const string VersionText = "10.0.100-preview.2";
 }
