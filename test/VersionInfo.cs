4668c57a779146accfe41b56860ae25361f3e711
jsakamoto <junichi.jp@gmail.com>
2025-07-09 08:41:18 +0900

    Update package references to versions 9.0.7 and 8.0.18

--- test/VersionInfo.cs
+++ test/VersionInfo.cs
@@ -1,5 +1,5 @@
 namespace Toolbelt.AspNetCore.Blazor.Minimum.Templates.Test;
 internal static class VersionInfo
 {
-    internal const string VersionText = "9.0.301";
+    internal const string VersionText = "9.0.302";
 }
